8634a8c46e82267f78bee9fbd97ac152e77835ac
João Teixeira <joao@indielayer.com>
2020-05-15 14:29:35 +0100

    feat: remove web firemodel

--- lib/core/src/web/core.ts
+++
@@ -1,7 +0,0 @@
-import * as _firebase from 'firebase/app'
-import 'firebase/firestore'
-
-export const firebase = _firebase
-export const db = firebase.firestore
-export const timestamp = firebase.firestore.Timestamp.now
-export const { serverTimestamp } = firebase.firestore.FieldValue
