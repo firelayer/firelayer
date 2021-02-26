4fed5c36b87cf807ec4c77e5316f3f6d587edf6c
João Teixeira <joao@indielayer.com>
2021-02-26 17:24:38 +0000

    feat: update firebase packages

--- lib/cli/src/helpers/createServiceAccount.ts
+++ lib/cli/src/helpers/createServiceAccount.ts
@@ -1,27 +1,27 @@
-import { createServiceAccount } from 'firebase-tools/lib/gcp/iam'
-import { grantRoles } from 'firebase-tools/lib/extensions/rolesHelper'
+// import { createServiceAccount } from 'firebase-tools/lib/gcp/iam'
+// import { grantRoles } from 'firebase-tools/lib/extensions/rolesHelper'
 
-export default async (): Promise<any> => {
-  const random5chars = Math.random().toString(36).substring(8)
+// export default async (): Promise<any> => {
+//   const random5chars = Math.random().toString(36).substring(8)
 
-  const projectId = 'firebase-boilerplate'
+//   const projectId = 'firebase-boilerplate'
 
-  const serviceAccount = await createServiceAccount(
-    projectId,
-    `firelayer-adminsdk-${random5chars}`,
-    'Firelayer Admin SDK',
-    'Firelayer CLI'
-  )
+//   const serviceAccount = await createServiceAccount(
+//     projectId,
+//     `firelayer-adminsdk-${random5chars}`,
+//     'Firelayer Admin SDK',
+//     'Firelayer CLI'
+//   )
 
-  await grantRoles(
-    projectId,
-    serviceAccount.email,
-    [
-      'storage.admin',
-      'datastore.databases.export',
-      'datastore.databases.import',
-      'firebase.admin',
-      'iam.serviceAccountTokenCreator'
-    ]
-  )
-}
+//   await grantRoles(
+//     projectId,
+//     serviceAccount.email,
+//     [
+//       'storage.admin',
+//       'datastore.databases.export',
+//       'datastore.databases.import',
+//       'firebase.admin',
+//       'iam.serviceAccountTokenCreator'
+//     ]
+//   )
+// }
